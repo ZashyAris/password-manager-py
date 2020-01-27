8f9f2ab8d41f8faf3b43db48983a1674438dea87
ZashyAris <sharonatieno080@gmail.com>
2020-01-27 13:26:23 +0300

    added  an empty array which are being desplaid in the object I've added

--- user.py
+++ user.py
@@ -2,9 +2,10 @@ class user:
     """
     Class that generates new instance of users
     """
-    pass
 
-       def __init__(self,user_name,password):
+    user_list = [] # Empty user list
+    
+    def __init__(self,user_name,password):
 
         '''
         __init__ method that helps us define properties for our objects.
@@ -13,3 +14,8 @@ class user:
             user_name: New user user_name.
             password: New user password.
         '''
+
+        self.user_name = user_name
+        self.password = password
+        
+        
