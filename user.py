2a3f663d31a846f22fbb4472beb0235a63e4fc9b
ZashyAris <sharonatieno080@gmail.com>
2020-01-27 12:22:25 +0300

    created the file user.py and made a user class in it

---
+++ user.py
@@ -0,0 +1,15 @@
+class user:
+    """
+    Class that generates new instance of users
+    """
+    pass
+
+       def __init__(self,user_name,password):
+
+        '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            user_name: New user user_name.
+            password: New user password.
+        '''
