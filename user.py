c12a508b7df4e4370a090f4bcdb145004d13e743
ZashyAris <sharonatieno080@gmail.com>
2020-01-28 11:04:00 +0300

    In the user.py file in the class User created a functions that are going to  save user, delete user,find user by username,check if a username exists and the laste one that displays the users

--- user.py
+++ user.py
@@ -1,4 +1,4 @@
-class user:
+class User:
     """
     Class that generates new instance of users
     """
@@ -17,5 +17,57 @@ class user:
 
         self.user_name = user_name
         self.password = password
+
+    def save_user(self):
+
+        '''
+        save_user method saves user objects into user_list
+        '''
+
+        User.user_list.append(self)
+
+    def delete_user(self):
+
+        '''
+        delete_user method deletes a saved user from the user_list
+        '''
+
+        User.user_list.remove(self)
         
+    @classmethod
+    def find_by_username(cls,username):
+        '''
+        Method that takes in a name and returns a user that user name .
+
+        Args:
+            name: User name to search for
+        Returns :
+            User of person that matches the user name.
+        '''
+
+        for user in cls.user_list:
+            if user.user_name == username:
+                return user
+            
+    @classmethod
+    def user_exist(cls,username):
+        '''
+        Method that checks if a user name exists from the user list.
+        Args:
+            name: User name to search if it exists
+        Returns :
+            Boolean: True or false depending if the user name exists
+        '''
+        for user in cls.user_list:
+            if user.user_name == username:
+                    return True
+
+        return False
+    
+    @classmethod
+    def display_user(cls):
+        '''
+        method that returns the user list
+        '''
+        return cls.user_list
         
